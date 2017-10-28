eb2f2bac83d2a94c810f2580d47129f165c173da
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-10-28 17:40:37 -0600

    setting up replace method

--- cbv_lambda_calc.hs
+++ cbv_lambda_calc.hs
@@ -1,9 +1,29 @@
 -- type Variable = String
 -- data Variable = Var Char deriving(Show)
-data Term = Var String
-  | Lambda String Term
-  | Application Term Term
-  deriving (Show)
 
--- subst::Term -> Term -> Term
--- subst Abstraction a b = (Var b) (Var a)
+import Data.List
+replace :: String -> Char -> [Char] -> String
+-- replace word ch ch_new = map (\letter -> if letter == ch then ch_new else letter) word
+-- replace word ch ch_new = if  Data.List.elem ch word
+--   then replace
+--   else word
+replace (x:xs) ch ch_new = if x == ch
+  then ch_new ++ xs
+  else x:(replace xs ch ch_new)
+-- https://stackoverflow.com/questions/19545253/haskell-replace-characters-in-string
+
+data Term = Var String
+  | Lambda Char Term
+  | Application Term Term
+  deriving (Show, Eq)
+
+-- subst::String -> Term -> Term -> Term
+-- [x -> t1] t2
+-- replace all free occurences of x in t1 by t2
+-- subst s (Lambda a (Var b)) (Var f) = Var x where
+--   x = replace b a (f !! 0)
+
+
+a = Var "xy"
+t1 = Lambda 'x' a
+t2 = Var "x"
