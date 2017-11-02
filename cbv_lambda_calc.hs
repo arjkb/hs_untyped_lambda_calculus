cbe851c15a4c7afe7a326e8684ff7602d34d55c0
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-01 21:31:13 -0600

    comment out replace because we probably won't need it

--- cbv_lambda_calc.hs
+++ cbv_lambda_calc.hs
@@ -2,16 +2,16 @@
 -- data Variable = Var Char deriving(Show)
 
 import Data.List
-replace :: String -> Char -> [Char] -> String
--- replace word ch ch_new = map (\letter -> if letter == ch then ch_new else letter) word
--- replace word ch ch_new = if  Data.List.elem ch word
---   then replace
---   else word
-replace [] _ _ = []
-replace (x:xs) ch ch_new = if x == ch
-  then ch_new ++ (replace xs ch ch_new)
-  else x:(replace xs ch ch_new)
--- https://stackoverflow.com/questions/19545253/haskell-replace-characters-in-string
+-- replace :: String -> Char -> [Char] -> String
+-- -- replace word ch ch_new = map (\letter -> if letter == ch then ch_new else letter) word
+-- -- replace word ch ch_new = if  Data.List.elem ch word
+-- --   then replace
+-- --   else word
+-- replace [] _ _ = []
+-- replace (x:xs) ch ch_new = if x == ch
+--   then ch_new ++ (replace xs ch ch_new)
+--   else x:(replace xs ch ch_new)
+-- -- https://stackoverflow.com/questions/19545253/haskell-replace-characters-in-string
 
 data Term = Var String
   | Lambda String Term
