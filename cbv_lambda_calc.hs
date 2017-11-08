2508eb5bcfe6b7753f54ef71f90e075b87041d28
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-07 20:16:39 -0700

    adding comments

--- cbv_lambda_calc.hs
+++ cbv_lambda_calc.hs
@@ -1,20 +1,25 @@
 import Data.List
 
-data Term = Var String
-  | Lambda String Term
-  | Application Term Term
+data Term = Var String    -- variable
+  | Lambda String Term    -- abstraction
+  | Application Term Term -- application
   deriving (Show, Eq)
 
 subst :: Term -> Term -> Term -> Term
 -- [x -> s]y
+
+-- variable case
 subst (Var x) (Var y) s = if x == y
   then s
   else (Var y)
+
+-- application case
 subst x (Application t1 t2) s = Application (subst x t1 s) (subst x t2 s)
 
 -- subst x (Lambda y t) s = Lambda y (subst x t s) --problematic
 
 -- this is the capture-avoiding part
+-- abstraction case
 subst (Var x) (Lambda y t) (Var s) = if s == y
   then Var x  -- INCORRECT!
   else Lambda y (subst (Var x) t (Var s))
